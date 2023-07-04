e5547be2d13f04520467cd1e006be18891994c28
Szymon Murawski <szymon.murawski@gmail.com>
2023-07-04 22:48:22 +0200

    feat: display list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Hero} from '../hero';
+import    { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +9,19 @@ import {Hero} from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero = {
+  selectedHero?: Hero;
+  hero: Hero = {
     id: 1,
     name: 'Windstorm'
   }
+  heroes = HEROES;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
